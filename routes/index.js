b2e0759cbdfb685899b7d504e4130b187fb31fb6
jdrichardstech <jdrichardstech@gmail.com>
2020-03-02 08:05:29 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('main/home', { name: 'JD', item: 'cloths', amount: 50 });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('auth/signup');
+});
+
+module.exports = router;
